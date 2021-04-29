69941fabb132d6da71be8e55a2f21bda2e91732c
madisonfisher <mbfishe2@ncsu.edu>
2021-04-29 08:37:33 -0400

    added sales department search

--- queries.sql
+++ queries.sql
@@ -26,3 +26,13 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 SELECT first_name, last_name, sex
 	FROM employees
 	WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- search 6: sales department employees
+-- employee number, last name, first name, department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+	FROM employees AS e
+	JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+		JOIN departments AS d
+		ON de.dept_no = d.dept_no
+		WHERE d.dept_name = 'Sales';
