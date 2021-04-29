60f80dea630faa112b819580361ede9400ee49fd
madisonfisher <mbfishe2@ncsu.edu>
2021-04-28 21:43:16 -0400

    code to generate the next 4 tables

--- schema.sql
+++ schema.sql
@@ -9,3 +9,45 @@ CREATE TABLE salaries (
 	emp_no INTEGER PRIMARY KEY,
 	salary INTEGER NOT NULL
 );
+
+--create table 3 - departments
+CREATE TABLE departments (
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR(40) NOT NULL
+);
+
+--create table 4 - employees
+CREATE TABLE employees (
+	emp_no INTEGER PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
+	title_id VARCHAR(5),
+	FOREIGN KEY (title_id) REFERENCES titles(title_id),
+	birth_date DATE,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(1),
+	hire_date DATE
+);
+
+--create table 5 - dept_manager
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INTEGER PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+--create table 6 - dept_emp
+--there are repeats in each column so both need to be established as the primary key together 
+--composite key
+CREATE TABLE dept_emp (
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (emp_no) REFERENCES dept_manager(emp_no),
+	dept_no VARCHAR(4),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (dept_no) REFERENCES dept_manager(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
