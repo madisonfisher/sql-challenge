c67c20ce31e8e74796608ec2afc553ea911ccfe6
madisonfisher <mbfishe2@ncsu.edu>
2021-04-29 08:18:51 -0400

    search for employees hired in 1986

--- queries.sql
+++ queries.sql
@@ -3,3 +3,9 @@ SELECT e.emp_no, last_name, first_name, sex, salary
 FROM employees AS e
 	JOIN salaries AS s
 	ON e.emp_no = s.emp_no;
+
+-- search 2: employees that were hired in 1986
+-- first name, last name, hire date
+SELECT first_name, last_name, hire_date
+	FROM employees
+	WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
