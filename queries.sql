10e103b24ca630daf6690ff19ecf2ea76411e7dc
madisonfisher <mbfishe2@ncsu.edu>
2021-04-29 08:53:07 -0400

    added department search

--- queries.sql
+++ queries.sql
@@ -19,7 +19,15 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 		JOIN employees AS e
 		ON (dm.emp_no = e.emp_no);
 
-
+-- search 4: departments of each employee
+-- employee number, last name, first name, department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+	FROM employees AS e
+	JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+		JOIN departments AS d
+		ON de.dept_no = d.dept_no
+		ORDER BY e.emp_no;
 
 -- search 5: Hercules B.
 -- first name, last name, sex
