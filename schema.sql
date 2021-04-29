2f94a2a7e0c385d923b64dcd3f9f7f48e9e6588b
madisonfisher <mbfishe2@ncsu.edu>
2021-04-28 21:47:00 -0400

    updated dept_manager and dept_emp

--- schema.sql
+++ schema.sql
@@ -30,12 +30,14 @@ CREATE TABLE employees (
 );
 
 --create table 5 - dept_manager
+--add as composite key
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no INTEGER PRIMARY KEY,
+	emp_no INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 --create table 6 - dept_emp
@@ -45,9 +47,7 @@ CREATE TABLE dept_emp (
 	emp_no INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (emp_no) REFERENCES dept_manager(emp_no),
 	dept_no VARCHAR(4),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (dept_no) REFERENCES dept_manager(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
