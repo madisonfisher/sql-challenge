676062826d5cca983c89efb18aeed78e56caf50f
madisonfisher <mbfishe2@ncsu.edu>
2021-04-29 08:31:38 -0400

    added Hercules B. search

--- queries.sql
+++ queries.sql
@@ -19,3 +19,10 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 		JOIN employees AS e
 		ON (dm.emp_no = e.emp_no);
 
+
+
+-- search 5: Hercules B.
+-- first name, last name, sex
+SELECT first_name, last_name, sex
+	FROM employees
+	WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
